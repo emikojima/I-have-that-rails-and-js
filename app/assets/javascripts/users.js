7a98168fc41486e8ff3887941df37a83f541ebc0
Emi Ko <emi8emi@gmail.com>
2018-09-28 10:10:35 -0700

    add divs for js input in app layout, add next functionality, add styling to next button in stylesheets/application.css

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,25 +5,31 @@ $(function(){
 function getUsers() {
   $.get('/users.json', function(data) {
     var users = data
-    $('#js-container').empty()
-    $('#js-title').empty()
-    $('#js-title').html("<h2>" + "Community Members" + "</h2>")
+    clear()
+    $('#js-title').html("<h2>" + "∇ Community Members ∇"  + "</h2>")
     users.forEach((user) =>
-      $('#js-container').append(`<h3> <a id="${user.id }" onclick="userLink(${user.id})">◦ ${user.name} ◦</a></h3><p>${user.city}, ${user.state}</p>`)
+      $('#js-container').append(`<br><h3> <a id="${user.id }" onclick="userLink(${user.id})">◦ ${user.name} ◦</a></h3><p>${user.city}, ${user.state}</p>`)
     )
   })
+}
+
+function clear() {
+  $('#everything').empty()
+  $('#js-container').empty()
+  $('#js-title').empty()
+  $('#js-next').empty()
 }
 
 function userLink(id) {
     $.get(`/users/${id}.json`, function(data){
       var items = data.items
-      $('#js-title').empty()
-      $('#js-container').empty()
-      $('#js-title').html("<h2>" + "User: " + `${data.name}` + "</h2><h4>" + "Location: " + `${data.city}` + ", " +`${data.state}` + "</h4>")
+      clear()
+      $('#js-title').html("<h4>" + "Δ Δ Δ" + "</h4><h3>" + "User: " + `${data.name}` + "</h3><p>" + "Location: " + `${data.city}` + ", " +`${data.state}` + "</p> <h4>" + "∇ ∇ ∇" + "</h4><br><h4>" + "Items for Lending: " + "</h4><br>")
 
-items.forEach((item) =>
-$('#js-container').append(`<h3>◦ ${item.name} ◦</h3><h4> ◦ ${item.description} ◦</h4> <h4> ◦ ${item.available} ◦</h4>`))
+      items.forEach((item) =>
+      $('#js-container').append(`<h4>◦ ${item.name} ◦</h4><p> Description:  ${item.description} </p> <p> Availabile: ${item.available} </p><br>`))
 
+      $('#js-next').html(`<button id="${id + 1}" onclick="userLink(${id + 1})">   Next User </button>`)
     })
 }
 
